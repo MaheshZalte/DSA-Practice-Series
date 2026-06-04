bf2197a1894fd1d79a9ac5c92c78c84f9718ba52
MaheshZalte <maheshzalte20@gmail.com>
2026-06-04 09:52:16 +0530

    100X Hello world print

---
+++ Date_4_6_26/Hello.java
@@ -0,0 +1,14 @@
+package Date_4_6_26;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        }
+        System.out.println("100X Hello World Completed");
+    }
+
+}
