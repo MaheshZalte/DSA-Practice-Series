b00665d93c7db6e3bc41d90a975390530814ce04
MaheshZalte <maheshzalte20@gmail.com>
2026-06-05 18:18:49 +0530

    Written code for checking prime number

--- Date_june/SumOfN.java
+++ Date_june/SumOfN.java
@@ -6,14 +6,34 @@ public class SumOfN {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // int counter = 1;
+        // while (counter <= n) {
+        //     sum += counter;
+        //     counter++;
+        // }
+        // System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+
+        // for loop
+        // for (int i = 1; i < 5; i++) {
+        //     for (int j = 1; j < 5; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        // reverse number 
         int n = sc.nextInt();
-        int sum = 0;
-        int counter = 1;
-        while (counter <= n) {
-            sum += counter;
-            counter++;
+        int reverse = 0;
+        while (n > 0) {
+
+            int lastDigit = n % 10;
+            reverse = reverse * 10 + lastDigit;
+            n /= 10;
+
         }
-        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+        System.out.println("Reverse of the number is: " + reverse);
+
     }
 
 }
